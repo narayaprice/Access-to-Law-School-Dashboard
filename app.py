3d3b22127b244694a85054e089b5c54c3e601d08
narayaprice <naraya.price@gmail.com>
2026-02-10 15:22:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import plotly.express as px
 # PAGE CONFIG
 # ============================================================
 st.set_page_config(page_title="Access to Law School Dashboard", layout="wide")
-
+st.sidebar.caption("BUILD: 2026-02-10_15:12")  # change this string every push
 # ============================================================
 # NAVY + WHITE THEME (MAIN + SIDEBAR)
 # ============================================================
